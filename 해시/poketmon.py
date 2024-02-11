a25b1e32353e6f33e708040876d79d98d43a2a78
하유정 <122142488+howdoujung@users.noreply.github.com>
2024-02-11 19:06:25 +0900

    Update poketmon.py

--- 해시/poketmon.py
+++ 해시/poketmon.py
@@ -2,8 +2,8 @@ def solution(nums):
     #해시 이용
     #dictionary: 키값이 중복될 수 없다(key-value)
     
-    #key값으로 만들어주는 작업
     num_len = len(nums)
+    #key값으로 만들어주는 작업
     change_key = dict.fromkeys(nums)
     #해시 상태를 리스트로 변환하는 작업 후 길이 반환
     answer = len(list(change_key))
